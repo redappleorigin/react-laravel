0a496905f4021711ac8547e13e3e37b91fdbdbeb
redappleorigin <johnrpelt@gmail.com>
2016-07-21 22:57:43 -0500

    Added proptypes as v8js will just show an empty page if a prop is used but not provided

--- resources/react/components/Csrf/Field/Field.js
+++ resources/react/components/Csrf/Field/Field.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 const Field = ({csrf}) => (
     <input type="hidden" name="_token" value={ csrf.token } />
 );
+
+Field.propTypes = {
+    csrf: PropTypes.object.isRequired,
+};
 
 export default connect((state) => (state))(Field);
--- resources/react/pages/Register/Register.js
+++ resources/react/pages/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import { connect } from 'react-redux';
 
@@ -92,5 +92,10 @@ const Register = (props) => (
         </div>
     </div>
 );
+
+Register.propTypes = {
+    session: PropTypes.object.isRequired,
+    validation: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+};
 
 export default connect((state) => state)(Register);
